6d9626e0cef874d657e7e1693a2f67b13d1f718b
Berczelly Attila <berczellyatti@gmail.com>
2021-03-14 23:43:07 +0100

    Task 3.5: State machine events based on input string.

--- hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/RunStatechart.java
+++ hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/RunStatechart.java
@@ -1,31 +1,63 @@
 package hu.bme.mit.yakindu.analysis.workhere;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 
-// import hu.bme.mit.yakindu.analysis.RuntimeService;
-// import hu.bme.mit.yakindu.analysis.TimerService;
+import hu.bme.mit.yakindu.analysis.RuntimeService;
+import hu.bme.mit.yakindu.analysis.TimerService;
 import hu.bme.mit.yakindu.analysis.example.ExampleStatemachine;
 import hu.bme.mit.yakindu.analysis.example.IExampleStatemachine;
 
-
-
 public class RunStatechart {
 	
+	public static void inputListener(IExampleStatemachine s) throws IOException {
+		
+		Method[] smethods = s.getSCInterface().getClass().getMethods();
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+		while(true) {
+			String input = reader.readLine();
+			input = input.toLowerCase();
+			
+			if(input.equals("exit")) {
+				return;
+			}else {
+				String methodname = "raise"+input;
+				
+				for(Method m : smethods) {
+					if(m.getName().toLowerCase().equals(methodname)) {
+						try {
+							m.invoke(s.getSCInterface());
+						} catch (IllegalAccessException e) {
+							e.printStackTrace();
+						} catch (IllegalArgumentException e) {
+							e.printStackTrace();
+						} catch (InvocationTargetException e) {
+							e.printStackTrace();
+						}
+						break;
+					}
+				}
+			}
+			print(s);
+	}
+}
+	
 	public static void main(String[] args) throws IOException {
-//		ExampleStatemachine s = new ExampleStatemachine();
-//		s.setTimer(new TimerService());
-//		RuntimeService.getInstance().registerStatemachine(s, 200);
-//		s.init();
-//		s.enter();
-//		s.runCycle();
-//		print(s);
-//		s.raiseStart();
-//		s.runCycle();
-//		System.in.read();
-//		s.raiseWhite();
-//		s.runCycle();
-//		print(s);
-//		System.exit(0);
+		ExampleStatemachine s = new ExampleStatemachine();
+		s.setTimer(new TimerService());
+		RuntimeService.getInstance().registerStatemachine(s, 200);
+		s.init();
+		s.enter();
+		s.runCycle();
+		inputListener(s);
+		System.exit(0);
+		
 	}
 
 	public static void print(IExampleStatemachine s) {
